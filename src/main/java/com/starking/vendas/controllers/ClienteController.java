1b74c52184fc55a506778ebf3c6af7477e8c12d7
pedrorhamon <pedrorhamon16@gmail.com>
2022-02-08 20:13:12 -0300

    Metodo de deletar

--- src/main/java/com/starking/vendas/controllers/ClienteController.java
+++ src/main/java/com/starking/vendas/controllers/ClienteController.java
@@ -1,8 +1,11 @@
 package com.starking.vendas.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.starking.vendas.entities.Cliente;
@@ -16,7 +19,12 @@ public class ClienteController {
 	private ClienteService clienteService;
 	
 	@PostMapping
-	public void salvar(Cliente cliente) {
+	public void salva(@RequestBody Cliente cliente) {
 		this.clienteService.salvar(cliente);
+	}
+	
+	@DeleteMapping
+	public void deletar(@RequestParam Long id) {
+		this.clienteService.deletar(id);
 	}
 }
--- src/main/java/com/starking/vendas/services/ClienteService.java
+++ src/main/java/com/starking/vendas/services/ClienteService.java
@@ -2,7 +2,6 @@ package com.starking.vendas.services;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.starking.vendas.entities.Cliente;
 import com.starking.vendas.repositories.ClienteRepository;
@@ -13,8 +12,12 @@ public class ClienteService {
 	private ClienteRepository clienteRepository;
 	
 	@Transactional
-	public void salvar(@RequestBody Cliente cliente) {
+	public void salvar(Cliente cliente) {
 		this.clienteRepository.save(cliente);
+	}
+	
+	public void deletar(Long clienteId) {
+		this.clienteRepository.deleteById(clienteId);
 	}
 
 }
