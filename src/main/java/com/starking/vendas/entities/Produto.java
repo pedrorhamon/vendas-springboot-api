daa59d985420886b11c068e111dec09f6d3c284b
pedrorhamon <pedrorhamon16@gmail.com>
2022-02-12 11:48:27 -0300

    mapeando entidade produto

--- src/main/java/com/starking/vendas/entities/Produto.java
+++ src/main/java/com/starking/vendas/entities/Produto.java
@@ -2,6 +2,13 @@ package com.starking.vendas.entities;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,11 +19,18 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @EqualsAndHashCode
+@Entity
+@Table(name = "produto")
 public class Produto {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Long id;
 	
+	@Column(name = "descricao")
 	private String descricao;
 	
+	@Column(name = "preco_unitario")
 	private BigDecimal preco;
 }
