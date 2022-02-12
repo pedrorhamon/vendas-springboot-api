231b0c23f15939f6efd0286fb0ca607264ce5c66
pedrorhamon <pedrorhamon16@gmail.com>
2022-02-12 20:08:00 -0300

    Criando a entidade itemPedido e seu mapeamento

--- src/main/java/com/starking/vendas/entities/ItemPedido.java
+++ src/main/java/com/starking/vendas/entities/ItemPedido.java
@@ -1,5 +1,17 @@
 package com.starking.vendas.entities;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -9,13 +21,26 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @EqualsAndHashCode
+@Entity
+@Table(name = "item_pedido")
 public class ItemPedido {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Long id;
 	
+	@ManyToOne
+	@JoinColumn(name = "pedido_id")
 	private Pedido pedido;
 	
+	@ManyToOne
+	@JoinColumn(name = "produto_id")
 	private Produto produto;
 	
+	@Column(name = "quantidade")
 	private Integer quantidade;
+	
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> items;
 }
