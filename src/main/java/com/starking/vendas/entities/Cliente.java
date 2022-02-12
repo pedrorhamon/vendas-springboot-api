97f852ff253221039ba1a773c672da34f494a1a8
pedrorhamon <pedrorhamon16@gmail.com>
2022-02-12 11:55:10 -0300

    map na entity pedido

--- src/main/java/com/starking/vendas/entities/Cliente.java
+++ src/main/java/com/starking/vendas/entities/Cliente.java
@@ -1,10 +1,13 @@
 package com.starking.vendas.entities;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -31,5 +34,7 @@ public class Cliente {
 	@Column(name = "nome")
 	@Size(min = 3, max = 100)
 	private String nome;
-
+	
+	@OneToMany(mappedBy = "cliente")
+	private Set<Pedido> pedidos;
 }
--- src/main/java/com/starking/vendas/entities/Pedido.java
+++ src/main/java/com/starking/vendas/entities/Pedido.java
@@ -3,6 +3,15 @@ package com.starking.vendas.entities;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -12,14 +21,23 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @EqualsAndHashCode
+@Entity
+@Table(name = "pedido")
 public class Pedido {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Long id;
 	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 	
+	@Column(name = "data_pedido")
 	private LocalDate dataPedido;
 	
+	@Column(name = "total", length = 20, precision = 2)
 	private BigDecimal total;
 
 }
