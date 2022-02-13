826e81d7e5e52471a8d43877aa4a1908bfc9aacb
pedrorhamon <pedrorhamon16@gmail.com>
2022-02-13 18:41:50 -0300

    Carregando pedidos de cliente

--- src/main/java/com/starking/vendas/repositories/PedidoRepository.java
+++ src/main/java/com/starking/vendas/repositories/PedidoRepository.java
@@ -1,9 +1,14 @@
 package com.starking.vendas.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.starking.vendas.entities.Cliente;
 import com.starking.vendas.entities.Pedido;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+	
+	List<Pedido> findByCliente(Cliente cliente);
 
 }
