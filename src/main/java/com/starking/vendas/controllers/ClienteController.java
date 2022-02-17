23fdf65306a4a5285ae1f75c94d1f7f2dba414d2
pedrorhamon <pedrorhamon16@gmail.com>
2022-02-17 19:24:24 -0300

    delete mapping

--- src/main/java/com/starking/vendas/controllers/ClienteController.java
+++ src/main/java/com/starking/vendas/controllers/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.starking.vendas.entities.Cliente;
@@ -50,6 +52,7 @@ public class ClienteController {
 	}
 	
 	@DeleteMapping
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	public void deletar(@PathVariable Long id) {
 		this.clienteService.deletar(id);
 	}
