7e65fdfc13a91f66a030f098be0ecb7b004f2c0b
pedrorhamon <pedrorhamon16@gmail.com>
2022-04-04 21:12:06 -0300

    Testando a autenticação com cadastro de usuarios

--- src/main/java/com/starking/vendas/config/SecurityConfig.java
+++ src/main/java/com/starking/vendas/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.starking.vendas.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -39,7 +40,10 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
 		.hasAnyRole("USER", "ADMIN")
 		.antMatchers("/api/produtos/**")
 		.hasRole("ADMIN")
-		.and()
+		.antMatchers(HttpMethod.POST, "/api/usuarios/**")
+			.permitAll()
+		.anyRequest().authenticated()
+	.and()
 		.httpBasic();
 	}
 
